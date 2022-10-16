ceb547a8501176f98439280695af22b2e3446bb8
Aidan Smith <aidan.w.smith@outlook.com>
2022-10-16 19:54:02 +0800

    if redPlayerIsHuman

--- src/Training.java
+++ src/Training.java
@@ -101,6 +101,9 @@ public class Training {
 
     public static void makeProbabilisticDecisionTrees(GameState game, boolean bluePlayerIsHuman, boolean redPlayerIsHuman) {
         if (bluePlayerIsHuman) {
+
+        }
+        if (redPlayerIsHuman) {
             
         }
     }
